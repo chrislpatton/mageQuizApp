fbc1401055caa45c757c1a50da6cd01c91505c26
chrislpatton <chrislpatton@gmail.com>
2016-10-09 13:04:13 -0400

    updaed server.js packae.json

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-// Define the port to run on
-app.set('port', 3000);
+var port = process.env.PORT || 3030;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
 // Listen for requests
-var server = app.listen(app.get('port'), function() {
-  var port = server.address().port;
-  console.log('Magic happens on port ' + port);
-});
+app.listen(port); 
+console.log('Server running on port ' + port);       // shoutout to the user
+exports = module.exports = app
